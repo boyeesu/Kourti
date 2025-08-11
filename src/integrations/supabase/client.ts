5dfe3f7cdf7ca76169db5c1c9b5987ba6fdcd1ac
Daniel Esuga <danielesuga@gmail.com>
2025-08-11 16:56:00 +0100

    chore: add supabase env example

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,12 @@
 import { createClient } from '@supabase/supabase-js';
 // import type { Database } from './types';
 
-const SUPABASE_URL = "https://zjbvnvydgsxqmmrrmvif.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqYnZudnlkZ3N4cW1tcnJtdmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODYzMTAsImV4cCI6MjA2OTY2MjMxMH0.-lE-O7iPZM_fxM93ddDapJVzcPdBArdCmN1HrwCHIH4";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
+  throw new Error('Missing Supabase environment variables');
+}
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
